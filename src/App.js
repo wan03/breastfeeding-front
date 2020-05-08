c6e5c1663b00d171c847d371d87fd2318daf858c
ikpb <ikpb@users.noreply.github.com>
2020-05-08 15:13:29 -0400

    added addphots page and added to routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import firebase from 'firebase';
-import service from './services/servicetest';
-import Routes from './services/router';
-import Navbar from './components/navbar/navbar';
+import React from "react";
+import "./App.css";
+import firebase from "firebase";
+import service from "./services/servicetest";
+import Routes from "./services/router";
+import Navbar from "./components/navbar/navbar";
 
-console.log(service.add(4,3));
+console.log(service.add(4, 3));
 
-  // Set the configuration for your app
-  const firebaseConfig = {
-    apiKey: "AIzaSyCNXsTrF1Z-9NqIJCuicxJ_WisfwjdAw2k",
-    authDomain: "breastfeeding-app-447b5.firebaseapp.com",
-    databaseURL: "https://breastfeeding-app-447b5.firebaseio.com",
-    projectId: "breastfeeding-app-447b5",
-    storageBucket: "breastfeeding-app-447b5.appspot.com",
-    messagingSenderId: "1061221728275",
-    appId: "1:1061221728275:web:27c620f0d7fb8e2b9b3fe6"
-  };
+// Set the configuration for your app
+const firebaseConfig = {
+  apiKey: "AIzaSyCNXsTrF1Z-9NqIJCuicxJ_WisfwjdAw2k",
+  authDomain: "breastfeeding-app-447b5.firebaseapp.com",
+  databaseURL: "https://breastfeeding-app-447b5.firebaseio.com",
+  projectId: "breastfeeding-app-447b5",
+  storageBucket: "breastfeeding-app-447b5.appspot.com",
+  messagingSenderId: "1061221728275",
+  appId: "1:1061221728275:web:27c620f0d7fb8e2b9b3fe6",
+};
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div>
-    <Navbar/>
-    <Routes/>
+      <Navbar />
+      <Routes />
     </div>
   );
 }
--- src/services/router.js
+++ src/services/router.js
@@ -1,31 +1,29 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 //Pages
-import Home from '../pages/home/home';
-import Contact from '../pages/contact/contact';
-import Dashboard from '../pages/dashboard/dashboard';
-
+import Home from "../pages/home/home";
+import Contact from "../pages/contact/contact";
+import Dashboard from "../pages/dashboard/dashboard";
+import AddPhoto from "../pages/add-photo/addphoto";
 
 export default function Routes() {
   return (
     <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/addphoto">
+          <AddPhoto />
+        </Route>
+      </Switch>
     </Router>
   );
 }
