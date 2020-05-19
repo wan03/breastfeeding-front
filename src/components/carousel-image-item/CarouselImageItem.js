815cf5477887914a9192a4289a03beb64dbaed21
wan03 <38962452+wan03@users.noreply.github.com>
2020-05-19 15:48:18 -0400

    Slide show is working with both images and videos

---
+++ src/components/carousel-image-item/CarouselImageItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { MDBCarouselItem, MDBView } from "mdbreact";
+
+
+/**
+ * Reusable Carousel Image Item Component.
+ * @param index
+ * @param downloadURL
+ * @param name
+ */
+const CarouselImageItem = (images) => (
+    <>
+    {images.map( image => {
+   return (
+    <MDBCarouselItem itemId={image.index} key={image.index}>
+        <MDBView>
+            <img
+            className="d-block w-100"
+            src={image.downloadURL}
+            alt={image.name}
+            />
+        </MDBView>
+    </MDBCarouselItem>
+    )} )};
+  </>
+  );
+
+
+export default CarouselImageItem;
--- src/pages/slideshow/Slideshow.js
+++ src/pages/slideshow/Slideshow.js
@@ -1,56 +1,121 @@
-import React from 'react';
-import { Carousel, ResponsiveEmbed } from "react-bootstrap"
+import React, { useRef, useState, useEffect, useReducer } from 'react';
+import { useAuth } from "../../config/Auth";
+import { storage } from "../../config/firebase";
+import CarouselImageItem from "../../components/carousel-image-item/CarouselImageItem"
+import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView, MDBContainer } from "mdbreact";
+
+const imageReducer = (images, payload) => {
+    return [...images, payload]
+}
+
+const videoReducer = (videos, payload) => {
+    return [...videos, payload]
+}
 
 function Slideshow() {
 
+    const slideshow = useRef(),
+    auth = useAuth(),
+    user = auth.user,
+    imageTypeCheck = /^image\/.*/,
+    videoTypeCheck = /^video\/.*/,
+    initialImagesValue = [],
+    initialVideosValue = [],
+    [images, dispatchImage] = useReducer(imageReducer, initialImagesValue),
+    [videos, dispatchVideo] = useReducer(videoReducer, initialVideosValue),
+    [lenght, setLength] = useState(0)
+
+
+    const nextSlide = () => {
+        slideshow.current.next()
+      };
+
+
+
+
+
+      useEffect(() => {
+        
+        const getUserMedia = () => {           
+            const storageLocation = storage.ref(`userData/${user.uid}`);
+            storageLocation.listAll().then(res => {
+               setLength(res.items.length);
+               console.log(res.items.length)
+               res.items.forEach((itemRef, index) => {
+                   itemRef.getMetadata().then(metadata => {
+                    if (imageTypeCheck.test(metadata.contentType)){
+                        let imageData = {
+                            index: index + 1,
+                            downloadURL: "",
+                            name: metadata.name
+                        }
+                    itemRef.getDownloadURL().then(res => {imageData.downloadURL = res
+                    
+                        dispatchImage(imageData);
+                    })
+                    } else if (videoTypeCheck.test(metadata.contentType)){
+                        let videoData = {
+                            index: index + 1,
+                            downloadURL: "",
+                            name: metadata.name,
+                            contentType: metadata.contentType
+                        }
+                    itemRef.getDownloadURL().then(res => {videoData.downloadURL = res
+                    
+                        dispatchVideo(videoData);
+                    })
+                    }
+                   }).catch(error => console.log(error))
+                   
+               }) 
+            })
+   }
+          if(user) {
+            getUserMedia();
+          }
+        
+      }, [user])
 
   return (
-
-
-<div className={"container"}>
-  <ResponsiveEmbed aspectRatio="16by9">
-  <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://via.placeholder.com/900x500"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://via.placeholder.com/900x500"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://via.placeholder.com/900x500"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-  </ResponsiveEmbed>
-</div>
-
-
- 
+  <MDBContainer>
+      <MDBCarousel
+        ref={slideshow}
+        activeItem={1}
+        length={lenght}
+        showControls={true}
+        showIndicators={false}
+        className="z-depth-1"
+        interval={false}
+      >
+        <MDBCarouselInner>
+        {images.map( image => {
+   return (
+    <MDBCarouselItem itemId={image.index} key={image.name}>
+        <MDBView>
+            <img
+            className="d-block w-100"
+            src={image.downloadURL}
+            alt={image.name}
+            />
+        </MDBView>
+    </MDBCarouselItem>
+    )} )};
+    {videos.map( video => {
+   return (
+    <MDBCarouselItem itemId={video.index} key={video.name}>
+        <MDBView>
+            <video
+            className="d-block video-fluid"
+            controls
+            >
+             <source src={video.downloadURL} type={video.contentType} />   
+            </video>
+        </MDBView>
+    </MDBCarouselItem>
+    )} )};
+        </MDBCarouselInner>
+      </MDBCarousel>
+    </MDBContainer>
   );
 }
 
