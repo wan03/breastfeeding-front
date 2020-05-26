f4e0e8c5e99c76b131a5b29feec6446c4a11d1af
wan03 <38962452+wan03@users.noreply.github.com>
2020-05-26 14:55:16 -0400

    update jumbotron.js to use state in preparation of headless cms

--- src/components/jumbotron/jumbotron.js
+++ src/components/jumbotron/jumbotron.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import {Jumbotron, Button } from 'react-bootstrap'
+import React, { useState } from "react";
+import { MDBJumbotron, MDBBtn } from "mdbreact";
 
 function JumbotronComponent() {
+
+  const [title, setTitle] = useState("Hello, world!"),
+  [body, setBody] = useState("This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information."),
+  [button, setButton] = useState("Learn more");
+
   return (
-<Jumbotron>
-  <h1>Hello, world!</h1>
-  <p>
-    This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.
-  </p>
-  <p>
-    <Button variant="primary">Learn more</Button>
-  </p>
-</Jumbotron>
+    <MDBJumbotron>
+      <h1>{title}</h1>
+      <p>
+        {body}
+      </p>
+      <p>
+        <MDBBtn variant="primary">{button}</MDBBtn>
+      </p>
+    </MDBJumbotron>
   );
 }
 
